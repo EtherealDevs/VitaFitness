724a0ae4dd29cc3be118195db6e177c7285a3137
kevin <kevin.galaxy04@gmail.com>
2025-02-21 10:20:31 -0300

    add Attendance, Classe, Student, Teacher, and TeacherSchedules models; create migrations for students, teachers, classes, attendances, and teacher schedules

---
+++ backend/database/migrations/2025_02_21_130745_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->date('registration_date');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('dni')->unique();
+            $table->foreignId('branches_id')->constrained('branches');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
---
+++ backend/database/migrations/2025_02_21_130904_create_teacher_schedules_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teacher_schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('teachers_id')->constrained('teachers');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->date('day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teacher_schedules');
+    }
+};
