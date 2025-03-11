3ab6f59eef8d7761f634603221fb17fb671dcd2d
kevin <kevin.galaxy04@gmail.com>
2025-03-11 11:48:28 -0300

    refactor: standardize formatting and improve prop handling in Button and Input components

--- frontend/src/components/ui/button.tsx
+++ frontend/src/components/ui/button.tsx
@@ -1,22 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    children: React.ReactNode
-    className?: string
+    children: React.ReactNode;
+    className?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({
-    children,
-    className,
-    ...props
-}: ButtonProps) => {
+const Button: React.FC<ButtonProps> = ({ children, className, ...props }: ButtonProps) => {
     return (
         <button
-            className={`px-4 py-2  bg-blue-500 rounded ${className}`}
-            {...props}>
+            className={`px-4 py-2 text-white bg-blue-500 rounded ${className}`}
+            {...props}
+        >
             {children}
         </button>
-    )
-}
+    );
+};
 
-export default Button
+export default Button;
