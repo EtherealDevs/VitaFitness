4610e952d518be2910ff10cdaa4916f160c45575
kevin <kevin.galaxy04@gmail.com>
2025-03-11 11:51:16 -0300

    refactor: improve button styling and standardize input props handling

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -1,21 +1,24 @@
-import type React from "react"
-import { type ChangeEvent, forwardRef } from "react"
+import type React from 'react'
+import { type ChangeEvent, forwardRef } from 'react'
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  id: string
-  type: string
-  name: string
-  value: string
-  className?: string
-  onChange: (event: ChangeEvent<HTMLInputElement>) => void
-  required?: boolean
-  autoFocus?: boolean
-  autoComplete?: string
+export interface InputProps
+    extends React.InputHTMLAttributes<HTMLInputElement> {
+    id: string
+    type: string
+    name: string
+    value?: string
+    className?: string
+    onChange?: (event: ChangeEvent<HTMLInputElement>) => void
+    required?: boolean
+    autoFocus?: boolean
+    autoComplete?: string
 }
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ autoComplete, ...props }, ref) => {
-  return <input ref={ref} autoComplete={autoComplete} {...props} />
-})
+const Input = forwardRef<HTMLInputElement, InputProps>(
+    ({ autoComplete, ...props }, ref) => {
+        return <input ref={ref} autoComplete={autoComplete} {...props} />
+    },
+)
 
-Input.displayName = "Input"
+Input.displayName = 'Input'
 export default Input
