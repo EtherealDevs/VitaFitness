653ed0fbf7bc71a254e9ca14eafa8da9d6afd00e
Rodrigo <gatopunk99@gmail.com>
2025-03-31 19:57:09 -0300

    Create TimeSlot

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClassScheduleTimeslotController;
 use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,5 +8,7 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 Route::get('testing', [TestController::class, 'index'])->name('testing');
+Route::post('/testing/timeslot', [ClassScheduleTimeslotController::class, 'store'])->name('testing.timeslot.store');
+Route::get('/testing/timeslot/create', [ClassScheduleTimeslotController::class, 'create'])->name('testing.timeslot.create');
 
 require __DIR__.'/auth.php';
