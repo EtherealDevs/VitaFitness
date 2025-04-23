1f20c9de446393c3b450409d4b4320644a879e57
Mateodans <mateodand@gmail.com>
2025-04-23 01:18:04 -0300

    refactor: mejorar el estilo del campo de búsqueda en el componente de gestión de estudiantes

--- frontend/src/app/admin/students/page.tsx
+++ frontend/src/app/admin/students/page.tsx
@@ -327,7 +327,7 @@ export default function StudentManagement() {
                                 placeholder="Buscar estudiantes..."
                                 value={searchTerm}
                                 onChange={e => setSearchTerm(e.target.value)}
-                                className="w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 dark:bg-[#363a3b] dark:border-slate-700 dark:text-white"
+                                className="w-full pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500/50  dark:bg-[#363a3b] dark:border-slate-700 dark:text-white"
                             />
                             <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
                         </div>
